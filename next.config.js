c6c1d8eaad624281ea875f23d2710f9eefbb0e57
henrix494 <henrix494@gmail.com>
2023-09-26 07:13:09 +0300

    added some style

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     experimental:{
         serverActions:true,
-    }
+    },
+ images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        
+        port: '',
+        pathname: '/**/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- src/components/main/CountryApi.tsx
+++ src/components/main/CountryApi.tsx
@@ -1,15 +1,17 @@
 "use client"
 import Card from "../UI/Card"
+import Image from "next/image"
 import { Country } from "@/types"
 import { useState,useEffect, Suspense } from "react"
 const baseUrl = "https://restcountries.com/v3.1/all"
 export default function CountryApi() {
     const [data,setData] = useState<Country[]>([]); 
-    const [isLoading,setIsLoading] = useState(true)
+    const [isLoading,setIsLoading] = useState(false)
 
     useEffect(() => {
-        
+        setIsLoading(true)
        const getData = async ()  => {
+          
             const firstData = await fetch(baseUrl)
             const json = await firstData.json()
             setData(json )
@@ -28,10 +30,10 @@ export default function CountryApi() {
             <>
            
             <Card>
-            <img src={item.flags.png}/>
-            <div className=" px-3 py-10 rounded-md  "> 
-                <p>{item.name.common}</p>
-                <p>{item.population}</p>
+            <Image src={item.flags.png} alt={item.name.common} className=" h-[200px]" width={300} height={400} />
+            <div className=" px-6 py-10 rounded-md  "> 
+                <p className=" font-bold"> {item.name.common}</p>
+                <p> <span>population: </span>{item.population}</p>
                 <p>{item.region}</p>
                 <p>{item.capital}</p>
             </div>
